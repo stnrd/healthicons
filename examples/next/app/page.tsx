9e82919424b660b8d6087dd72de200c6a8413c9f
Sten Raadschelders <sraadschelders@nicolab.com>
2024-05-13 09:43:46 +0200

    Adding Next example application for testing icons

---
+++ examples/next/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { BloodBag } from 'healthicons-react';
+
+
+export default function Home() {
+    return (
+        <>
+            <BloodBag fill='red' height={36} width={36} />
+        </>
+    )
+};
