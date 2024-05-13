e9b1354b8ddbfabf189cff516acc698fde61f1ec
Sten Raadschelders <sraadschelders@nicolab.com>
2024-05-13 12:57:15 +0200

    Adding React Native example application

---
+++ examples/react-native/App.js
@@ -0,0 +1,10 @@
+import { Bloodbag } from "healthicons-react-native";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Bloodbag />
+    </View>
+  );
+}
