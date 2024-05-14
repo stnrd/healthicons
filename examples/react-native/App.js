98d10a7d954aa7946e7c89c941c30611b3402b27
Sten Raadschelders <s.raadschelders@gmail.com>
2024-05-14 20:08:45 +0200

    Adding Health icons provider to react and react native application

--- examples/react-native/App.js
+++ examples/react-native/App.js
@@ -1,10 +1,21 @@
-import { Bloodbag } from "healthicons-react-native";
 import { View } from "react-native";
+import { Bloodbag, DiabetesPeople } from "healthicons-react-native/dist/filled";
+import { HealthIconsProvider } from "healthicons-react/dist/HealthIconsContext";
 
 export default function App() {
   return (
     <View>
-      <Bloodbag />
+      <BloodBag fill="red" height={36} width={36} />
+      <HealthIconsProvider
+        iconProps={{
+          color: "#1E441E",
+          strokeWidth: 1,
+          width: "2em",
+          height: "2em",
+        }}
+      >
+        <DiabetesPeople fill="currentColor" />
+      </HealthIconsProvider>
     </View>
   );
 }
