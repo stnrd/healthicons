57cfc89204ab526650091d19cfe266f886c4fd98
Sten Raadschelders <sraadschelders@nicolab.com>
2024-10-28 08:39:00 +0100

    Adding vue example application

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {
+  Database,
+  HealthiconsProvider,
+  BloodBag,
+  OxygenTank,
+  ChartPie,
+  BloodBagFilled24px
+} from 'healthicons-vue';
+</script>
+
+<template>
+  <Database />
+  <OxygenTank color="red" height="36" width="36" />
+  <BloodBag />
+  <BloodBagFilled24px />
+
+  <HealthiconsProvider
+    :icon-props="{
+      'color': '#1E441E',
+      'stroke-width': 1,
+      'width': '2em',
+      'height': '2em',
+    }"
+  >
+    <SomeOtherContainer>
+      <ChartPie />
+    </SomeOtherContainer>
+  </HealthiconsProvider>
+</template>
