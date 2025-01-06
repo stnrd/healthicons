79ef8400038b300808a6477facc9142ae226c686
Sten Raadschelders <sraadschelders@nicolab.com>
2025-01-06 17:45:00 +0100

    Enable tree shaking on health-icons react and native

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
 // @ts-check
 import antfu from '@antfu/eslint-config';
-import nextPlugin from '@next/eslint-plugin-next';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-import reactPlugin from 'eslint-plugin-react';
-import hooksPlugin from 'eslint-plugin-react-hooks';
 
 export default antfu({
   typescript: true,
@@ -22,12 +18,7 @@ export default antfu({
   },
   ignores: [
     'css/*.css',
-    'iconoir.com/out/',
     '**/.expo/',
-    'packages/iconoir-flutter/.dart_tool/', 
-    'packages/iconoir-flutter/build/',
-    'packages/iconoir-flutter/example/',
-
   ],
   rules: {
     'style/padding-line-between-statements': [
@@ -69,48 +60,5 @@ export default antfu({
         ],
       },
     ],
-  },
-}, {
-  files: ['iconoir.com/**'],
-  plugins: {
-    '@next/next': nextPlugin,
-    'react': reactPlugin,
-    'react-hooks': hooksPlugin,
-    'jsx-a11y': jsxA11yPlugin,
-  },
-  settings: {
-    next: {
-      rootDir: 'iconoir.com/',
-    },
-    react: {
-      version: 'detect',
-    },
-
-  },
-  // @ts-ignore
-  rules: {
-    ...nextPlugin.configs.recommended.rules,
-    ...nextPlugin.configs['core-web-vitals'].rules,
-    ...reactPlugin.configs.recommended.rules,
-    ...hooksPlugin.configs.recommended.rules,
-
-    // rules from "eslint-config-next"
-    'import/no-anonymous-default-export': 'warn',
-    'react/no-unknown-property': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image'],
-      },
-    ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'react/jsx-no-target-blank': 'off',
   },
 });
