8b14d3f3e0cd479368052a384bab0ae0b95f5e77
Sten Raadschelders <sraadschelders@nicolab.com>
2025-01-06 17:57:04 +0100

    Add visualizer to the example vue application

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -2,11 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    visualizer({
+      open: true, // Automatically open the bundle report
+      filename: "dist/stats.html",
+      gzipSize: true,
+      brotliSize: true,
+    })
   ],
   resolve: {
     alias: {
